be3d6ed964f8e586accb5027cd7a40272d98fe90
pt1012 <pt1012@ic.ac.uk>
2014-11-10 23:28:50 +0000

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Java1{
+    public static void main(String[] args){
+	  Scanner input = new Scanner(System.in);
+	
+	  String username;
+    System.out.println("Enter your GitHub username");
+	  username = input.next();
+	
+	  System.out.println("Your GitHub username is " + username);
+	  }
+}
